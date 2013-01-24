d3ef4cbc29500ba7c4a07340ceb6ac41d1043d7c
Kostas Agnantis <kagnadis@gmail.com>
2013-01-24 16:47:22 +0200

    devide events equally in one day slot

--- _js/site.js
+++ _js/site.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
+	
 	$("#resizable").resizable();
 	$(".draggable").draggable();
+	$(".cell").data("_children", []);
 	$(".cell").droppable({
 			over: function(event, ui) {
 				$(this).css("background-color", "#FDD471")
@@ -18,6 +20,8 @@ $(document).ready(function() {
 	$(".cell").droppable({
 			drop: function(event, ui) {
 				$(ui.draggable).resizable();
+				addChild($(this), $(ui.draggable));
+				alert("Post: " + $(this).data("_children").length);
 			}
 	});
 	
@@ -36,7 +40,9 @@ $(document).ready(function() {
 	//currently there is a bug with draggable and :active pseudo-class in firefox
 	//so I need to use mousedown, mouseup events, instead.
 	$("#add_event_btn").click(function() {
-			$("<div class=\"draggable\">Event 1</div>")
+			//request the name of the event
+			var userInput = prompt("Event Name", "");
+			$("<div class=\"draggable\">" + userInput + "</div>")
 			//.resizable({ disabled: true })
 			.draggable() //{snap: ".cell"})
 			.bind('mousedown', function() {
@@ -46,7 +52,43 @@ $(document).ready(function() {
       	$(this).css('box-shadow', '0px 0px 5px 2px rgba(101,101,101,.7)');
   		})
 			.appendTo("#draggable_container");
+			
 	});
 	
 	
 }); // end ready
+
+function addChild(cell, event) {
+	alert("Start: " + cell.data("_children").length);
+	var ch = cell.data("_children");
+	//alert($.inArray(event, ch));
+	if ($.inArray(event, ch) > -1)	 {
+		alert("Exists!");
+		return;
+	}
+	
+	ch.push(event);
+	
+	var ch_no = ch.length;
+	var offset = cell.offset();
+	var max_width = cell.width() - 4;
+	var child_width = (max_width/ch_no - (ch_no-1));
+	var ind = 0;
+	for (var i = 0; i < ch_no; i++) {
+		//alert(ch[i]);
+		ch[i].offset({ top: offset.top+2, left: ((offset.left+2) + i*child_width)})
+		ch[i].css("width", child_width + "px");		
+	}
+	
+	cell.data("_children", ch);
+	alert("End: " + cell.data("_children").length);
+}
+
+function testArray() {
+	var arr = [];
+	arr.push($("#add_event_btn")[0]);
+	arr.push("44");
+	alert("44 not exists: " + $.inArray("44", arr));
+	alert("btn exists: " + $.inArray($("#add_event_btn1")[0], arr));
+}
+	
